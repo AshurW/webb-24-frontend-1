5a69e2305223258f6f2813254c5975d56f75664e
AshurW <ashurw@gmail.com>
2024-10-03 14:19:16 +0200

    Add fetch and localstorage

---
+++ js/fetch/script.js
@@ -0,0 +1,99 @@
+// const theFetch = fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Något gick fel");
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // console.log(data)
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error("Fel:", error);
+//   });
+
+async function fetchData() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function getData () {
+    return fetchData()
+}
+
+console.log(getData())
+
+//   console.log('the fetch', theFetch)
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   body: JSON.stringify({
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   }),
+//   headers: {
+//     "Content-type": "application/json; charset=UTF-8",
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//     method: 'PUT',
+//     body: JSON.stringify({
+//     //   id: 1,
+//       title: 'foo',
+//     //   body: 'bar',
+//       userId: 1,
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//     method: 'DELETE',
+//   });
+
+// console.log('first hi')
+
+// const minPromise = new Promise((resolve, reject) => {
+//     // Gör något asynkront här...
+//     const operationSuccess = true
+
+//     const sum = 1 + 1
+
+//     if (operationSuccess) {
+//       resolve(sum); // Om allt gick bra
+//     } else {
+//       reject('Operationen misslyckades'); // Om något gick fel
+//     }
+//   });
+
+
+// minPromise
+// .then(data => {
+//     console.log(data); // Om operationen lyckades
+// })
+// .catch(error => {
+//     console.error(error); // Om operationen misslyckades
+// }); 
+
+// console.log('second hej')
+
+// try {
+    
+// } catch (error) {
+    
+// }
---
+++ js/localstorage/script.js
@@ -0,0 +1,46 @@
+// Spara data i localStorage
+// const myArray = [1,2,3]
+
+// const myObject = {'name': 'ashur', 'age': 32}
+
+// console.log('my array type', myArray, typeof myArray)
+// console.log('my myObject type', myObject, typeof myObject)
+
+// const username = "ashur"
+
+// localStorage.setItem('username', 'JohnDoe');
+// localStorage.setItem('email', 'john@example.com');
+// localStorage.setItem('number', myArray);
+// localStorage.setItem('myobject', myObject);
+
+// // Hämta data från localStorage
+// const username = localStorage.getItem('username');
+// const email = localStorage.getItem('email');
+// const number = localStorage.getItem('number');
+
+// console.log('username', username, typeof username)
+// console.log('email', email, typeof email)
+// console.log('number', number, typeof number)
+// console.log('myObject', myObject, typeof myObject)
+
+// Ta bort en specifik post från localStorage
+// localStorage.removeItem('email');
+
+// Rensa hela localStorage
+// localStorage.clear();
+
+
+// Spara data i sessionStorage
+// sessionStorage.setItem('theme', 'dark');
+// sessionStorage.setItem('language', 'en');
+// sessionStorage.setItem('test', 123)
+
+// // Hämta data från sessionStorage
+// const theme = sessionStorage.getItem('theme');
+// const language = sessionStorage.getItem('language');
+
+// // Ta bort en specifik post från sessionStorage
+// sessionStorage.removeItem('language');
+
+// Rensa hela sessionStorage
+// sessionStorage.clear();
